3d866c45877e5f2a099d7388a177e9abcefc4f35
klelmira <70431965+klelmira@users.noreply.github.com>
2023-04-29 20:57:23 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,9 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+   
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing GOOD <code>pages/index.js</code>
         </p>
       </main>
 
